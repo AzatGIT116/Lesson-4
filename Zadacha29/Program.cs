3d117de2141ba2f146e290dfae6ef35ca88e799c
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-07 22:05:01 +0300

    add Zadacha 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+    Random myRan = new Random();
+        for (int i = 0; i < array.Length; i++)
+            array[i] = myRan.Next(0, 50);
+            Console.WriteLine("[{0}]", string.Join(", ", array));
+        
+      
+
+
