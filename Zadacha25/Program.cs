bfabe01d0efe3c2435786794b8244ff4db27c569
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-07 22:04:19 +0300

    add Zadacha 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B");
+double B = Convert.ToDouble(Console.ReadLine());
+double N = 0;
+N = Math.Pow (A, B); 
+Console.WriteLine(N);
+
